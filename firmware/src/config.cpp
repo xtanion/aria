db775c5951882b1e6c1c0a3003e474eeccf8bfe5
xtanion <shivam@edralabs.com>
2025-06-25 01:46:14 +0530

    Ignore firmware/src/config.cpp in Git and delete file

--- firmware/src/config.cpp
+++
@@ -1,7 +0,0 @@
-#include "config.h"
-
-const char* ssid = "Son of Anton";
-const char* password = "yomamagay";
-
-const char* websocket_server = "10.159.192.122";
-const uint16_t websocket_port = 8000;
